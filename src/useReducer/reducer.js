0ad1553a0c432c30ec60ffac0cae5ee9d23041a5
Arian <Arianbod@outlook.com>
2024-01-15 02:13:55 +0300

    swtch case added

---
+++ src/useReducer/reducer.js
@@ -0,0 +1,45 @@
+import { vars } from './actions'
+export const reducer = (state, action) => {
+    let NewCart;
+    let ItemId;
+    let EditingItem;
+    let EditedItem;
+    switch (action.type) {
+        case vars.INCREASE_ITEM:
+            NewCart = new Map(state.cart)
+            ItemId = action.payload.id;
+            EditingItem = NewCart.get(ItemId)
+            EditedItem = {
+                ...EditingItem, amount: EditingItem.amount + 1
+            }
+            NewCart.set(ItemId, EditedItem)
+            return { ...state, cart: NewCart }
+
+            break;
+        case vars.DECREASE_ITEM:
+
+            NewCart = new Map(state.cart)
+            ItemId = action.payload.id;
+            EditingItem = NewCart.get(ItemId)
+            EditedItem = { ...EditingItem, amount: EditingItem.amount - 1 }
+            EditedItem.amount > 0 ?
+                NewCart.set(ItemId, EditedItem) : NewCart.delete(ItemId)
+            return { ...state, cart: NewCart }
+            break;
+        case vars.REMOVE_ITEM:
+            NewCart = new Map(state.cart)
+            NewCart.delete(action.payload.id)
+            return { ...state, cart: NewCart }
+            break;
+        case vars.CLEAR_ITEMS:
+            return { ...state, cart: new Map() }
+            break;
+
+        default:
+            throw new Error(`no matching action type ${action.type}`)
+            break;
+    }
+
+}
+
+
