26ee34e5d8bbd54faffb8a8ae747c501817f3632
Arian <Arianbod@outlook.com>
2024-01-15 03:16:27 +0300

    conditional rendering added

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,22 @@
 // components
 import Navbar from './Navbar';
 import CartContainer from './CartContainer';
+import { useGlobalContext } from './Context';
 
 function App() {
-  return (
-    <main>
-      <Navbar />
-      <CartContainer />
-    </main>
-  );
+	const { isLoading } = useGlobalContext();
+
+	return (
+		<main>
+			{isLoading && <div className='loading'></div>}
+			{!isLoading && (
+				<>
+					<Navbar />
+					<CartContainer />
+				</>
+			)}
+		</main>
+	);
 }
 
 export default App;
