13800b7b63e453177cb8e61caeca2e9f898f1eba
Arian <Arianbod@outlook.com>
2024-01-15 03:17:00 +0300

    API replaced with fake data

--- src/Context.jsx
+++ src/Context.jsx
@@ -3,15 +3,13 @@ import { reducer } from './useReducer/reducer';
 import { vars } from './useReducer/actions';
 import cartItems from './data';
 import { getTotals } from './utils';
-
+const API = import.meta.env.VITE_API_URL;
 const cartItemsObject = cartItems.map((item) => [item.id, { ...item }]);
-const cartMap = new Map(cartItemsObject);
-const newArrayFromMap = Array.from(cartMap.entries());
 const AppContext = createContext();
-// const CLEARFunc
+
 const initialState = {
 	isLoading: false,
-	cart: cartMap,
+	cart: new Map(),
 };
 export const AppProvider = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
@@ -39,6 +37,23 @@ export const AppProvider = ({ children }) => {
 		totalAmount,
 		totalCost,
 	};
+	const FetchData = async () => {
+		dispatch({ type: vars.LOADING });
+		const response = await fetch(API);
+		if (!response.ok) {
+			throw new Error(`not responding - ${response.message}`);
+		}
+		try {
+			const ResData = await response.json();
+			dispatch({ type: vars.DISPLAY_ITEMS, payload: { ResData } });
+		} catch (error) {
+			console.error(error);
+		}
+	};
+
+	useEffect(() => {
+		FetchData();
+	}, []);
 	return <AppContext.Provider value={values}>{children}</AppContext.Provider>;
 };
 
